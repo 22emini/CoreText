9b67cfe5611bd3b2679606000205fc68ea9701c2
22emini <eminioluwaakinrinade716@example.com>
2025-04-29 11:37:37 +0100

    minor changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,16 @@ import { Button } from "./ui/button";
 const Hero = () => {
   return (
     <div className="px-4  md:px-12 flex flex-col   bg-gradient-to-br from-slate-900 via-slate-950 to-black justify-center  items-center h-[95vh] relative mx-auto text-center">
-      <h1 className="text-3xl md:text-[70px]  text-white lg:text-[78px] font-bold max-w-4xl">
-        Summarize Smarter, Not Harder ⚡
+      <h1 className="text-3xl md:text-[70px]  text-white lg:text-[78px] font-bold max-w-4xl">⚡ Instant Summaries, Zero Effort
       </h1>
       <p className="text-sm mt-2 md:text-base text-center text-white mb-8 max-w-4xl">
         Transform lengthy texts into clear, concise summaries instantly. Save
         time, stay informed, and focus on what truly matters with our AI-powered
-        summarizer.
+        summarizer. 📌 Save time. Stay sharp. Focus on what matters—without the fluff.
+
+(AI-powered. Lightning-fast. Smarter reading starts here.)
       </p>
+  
 
       <Link href="/summarize">
         <Button >Get Started</Button>
